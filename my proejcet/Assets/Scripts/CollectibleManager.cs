ce698068f431fcb1b9e382295b2e7a1673700a7d
Kevin T <126206698+kevintran120@users.noreply.github.com>
2024-06-18 10:34:48 +1000

    finished collectibles
    still placeholdesr
    also i have a placeholder door

--- my proejcet/Assets/Scripts/CollectibleManager.cs
+++ my proejcet/Assets/Scripts/CollectibleManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectibleManager : MonoBehaviour
 {
     public int collectibleCount;
+    public Text collectibleText;
+    public GameObject door;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,11 @@ public class CollectibleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        collectibleText.text = "Collectible Count: " + collectibleCount.ToString();
+
+        if(collectibleCount == 6)
+        {
+            Destroy(door);
+        }
     }
 }
