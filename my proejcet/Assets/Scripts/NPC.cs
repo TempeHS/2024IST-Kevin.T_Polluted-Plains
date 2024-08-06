f0ac0f9b681601ca3aa2bcc3e031642c366417b7
kevintran120 <126206698+kevintran120@users.noreply.github.com>
2024-08-06 11:55:00 +1000

    i b doin
    redoing the dialogue npcs stuff because it didnt work with multiple npcs
    https://www.youtube.com/watch?v=RfLCzDzkvb0

--- my proejcet/Assets/Scripts/NPC.cs
+++ my proejcet/Assets/Scripts/NPC.cs
@@ -5,98 +5,14 @@ using UnityEngine.UI;
 
 public class NPC : MonoBehaviour
 {
-    public GameObject dialoguePanel;
-    public GameObject interactPanel;
-    public Text dialogueText;
-    public string[] dialogue;
-    private int index;
 
-    public GameObject contButton;
-    public float wordSpeed;
-    public bool playerIsClose;
+    void Start()
+    {
+        
+    }
 
-
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E) && playerIsClose)
-        {
-            if(dialoguePanel.activeInHierarchy)
-            {
-                zeroText();
-            }
-            else
-            {
-                dialoguePanel.SetActive(true);
-                StartCoroutine(Typing());
-            }
-        } // pressing e to interact
 
-        if(dialogueText.text == dialogue[index])
-        {
-            contButton.SetActive(true);
-        }
-
-        if(playerIsClose)
-        {
-            interactPanel.SetActive(true);
-        }
-        else
-        {
-            interactPanel.SetActive(false);
-        }
-
-        if(dialoguePanel.activeInHierarchy)
-        {
-            interactPanel.SetActive(false);
-        }
-    }
-
-    public void zeroText()
-    {
-        dialogueText.text = "";
-        index = 0;
-        dialoguePanel.SetActive(false);
-    } // reset text (maybe)
-
-    IEnumerator Typing()
-    {
-        foreach(char letter in dialogue[index].ToCharArray())
-        {
-            dialogueText.text += letter;
-            yield return new WaitForSeconds(wordSpeed);
-        }
-    } // word speed for npc
-
-    public void NextLine()
-    {
-        contButton.SetActive(false);
-
-        if(index < dialogue.Length -1)
-        {
-            index++;
-            dialogueText.text = "";
-            StartCoroutine(Typing());
-        }
-        else
-        {
-            zeroText();
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            playerIsClose = true;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            playerIsClose = false;
-            zeroText();
-        }
     }
 }
