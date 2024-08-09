bfc349595e8848ff75c4761fdcdc45beb3416c7c
Kevin T <126206698+kevintran120@users.noreply.github.com>
2024-08-09 21:59:04 +1000

    dialogue mostly done
    thank bless
    but i need to make the continue button not work when i press space because i b jumpin but then the dialogue goes
    also uh make youserlf stop to talk to them
    and also make an interact button so you dojnt have to stop every time you go past them
    its in the same tutorail at the end here
    https://www.youtube.com/watch?v=DOP_G5bsySA

--- my proejcet/Assets/Scripts/dialgoue/DialogueManager.cs
+++ my proejcet/Assets/Scripts/dialgoue/DialogueManager.cs
@@ -1,42 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
-
+using TMPro;
+ 
 public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager Instance;
 
-    public Image CharacterIcon;
     public TextMeshProUGUI characterName;
     public TextMeshProUGUI dialogueArea;
-
+ 
     private Queue<DialogueLine> lines;
-
+    
     public bool isDialogueActive = false;
-
-    public float typingSpeed = 0.2f;
-
+ 
+    public float typingSpeed = 0.02f;
+ 
     public Animator animator;
-    void Start()
+ 
+    private void Awake()
     {
         if (Instance == null)
             Instance = this;
+ 
+        lines = new Queue<DialogueLine>();
     }
-
+ 
     public void StartDialogue(Dialogue dialogue)
     {
         isDialogueActive = true;
-        animator.Play("show");
+ 
+        animator.Play("in");
+ 
         lines.Clear();
-
+ 
         foreach (DialogueLine dialogueLine in dialogue.dialogueLines)
         {
             lines.Enqueue(dialogueLine);
         }
-
+ 
         DisplayNextDialogueLine();
     }
-
+ 
     public void DisplayNextDialogueLine()
     {
         if (lines.Count == 0)
@@ -44,23 +50,30 @@ public class DialogueManager : MonoBehaviour
             EndDialogue();
             return;
         }
-
+ 
         DialogueLine currentLine = lines.Dequeue();
 
-        characterIcon.sprite = currentLine.character.icon;
         characterName.text = currentLine.character.name;
-
+ 
         StopAllCoroutines();
-
+ 
         StartCoroutine(TypeSentence(currentLine));
     }
-
+ 
     IEnumerator TypeSentence(DialogueLine dialogueLine)
     {
         dialogueArea.text = "";
         foreach (char letter in dialogueLine.line.ToCharArray())
         {
-            
+            dialogueArea.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
         }
     }
+ 
+    void EndDialogue()
+    {
+        isDialogueActive = false;
+        animator.Play("out");
+    }
 }
+ 
--- my proejcet/Assets/Scripts/dialgoue/DialogueTrigger.cs
+++ my proejcet/Assets/Scripts/dialgoue/DialogueTrigger.cs
@@ -1,14 +1,12 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 [System.Serializable]
 public class DialogueCharacter
 {
     public string name;
-    public Sprite icon;
 }
-
+ 
 [System.Serializable]
 public class DialogueLine
 {
@@ -16,14 +14,27 @@ public class DialogueLine
     [TextArea(3, 10)]
     public string line;
 }
-
+ 
 [System.Serializable]
 public class Dialogue
 {
     public List<DialogueLine> dialogueLines = new List<DialogueLine>();
 }
-
+ 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+ 
+    public void TriggerDialogue()
+    {
+        DialogueManager.Instance.StartDialogue(dialogue);
+    }
+ 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            TriggerDialogue();
+        }
+    }
 }
