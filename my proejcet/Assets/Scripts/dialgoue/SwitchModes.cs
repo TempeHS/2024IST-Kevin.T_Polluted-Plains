476e92bbc83913bd08b72f548b597c39cc7c406b
kevintran120 <126206698+kevintran120@users.noreply.github.com>
2024-09-06 11:54:44 +1000

    oh bro
    the button doesnt work when you talk to the bee im gana tweak
    other than that its all good 😊

--- my proejcet/Assets/Scripts/dialgoue/SwitchModes.cs
+++ my proejcet/Assets/Scripts/dialgoue/SwitchModes.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SwitchModes : MonoBehaviour
 {
     public GameObject dialogueBox;
     public GameObject Continue;
+    public GameObject Interact;
     public Image dBoxImage;
+    public TextMeshProUGUI text;
 
     void Start()
     {
         Continue = dialogueBox.transform.GetChild(2).gameObject;
-        dBoxImage = gameObject.GetComponent<Image>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,6 +28,10 @@ public class SwitchModes : MonoBehaviour
     IEnumerator SwitchToNormal()
     {
         Continue.SetActive(true);
+        Interact.SetActive(true);
+        text.fontSize = 30;
+        text.color = Color.black;
+        text.alignment = TextAlignmentOptions.TopLeft;
         dBoxImage.color = new Color(255, 255, 255, 255);
         yield return new WaitForSeconds(0.1f);
         Destroy(gameObject);
